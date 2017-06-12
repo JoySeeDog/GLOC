beb3e5cc121ffdb2d531c862c88c2fc58017cdee
zhoujianquan <zhoujianquan@pwrd.com>
2017-06-12 14:39:36 +0800

    Initial Commit

---
+++ GLOC/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GLOC
+//
+//  Created by jianquan on 12/06/2017.
+//  Copyright © 2017 JoySeeDog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
